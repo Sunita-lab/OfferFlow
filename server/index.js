ac78d48d4f51623898ad7d9a653b5ec8697b84cc
SUNITA SATPATHY <sunitasatpathy80@gmail.com>
2026-05-26 14:37:21 +0530

    fix: add localhost to CORS origins

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'https://offer-flow-chi.vercel.app',
+  origin: ['https://offer-flow-chi.vercel.app', 'http://localhost:5173'],
   methods: ['GET', 'POST'],
 }));
 app.use(express.json({ limit: '50mb' }));
