583c70ea51235893d928daf43161c066516cb065
SUNITA SATPATHY <sunitasatpathy80@gmail.com>
2026-06-01 17:03:49 +0530

    fix: force IPv4 for SMTP connection on Render

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -7,7 +7,8 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
-  }
+  },
+  family: 4,
 });
 
 module.exports = transporter;
