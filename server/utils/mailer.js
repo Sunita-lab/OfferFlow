92578e055909fe2243cd20738af5b23f5190aa2f
SUNITA SATPATHY <sunitasatpathy80@gmail.com>
2026-06-01 17:09:45 +0530

    fix: switch to port 465 with SSL for SMTP

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -2,8 +2,8 @@ const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  secure: false,
+  port: 465,
+  secure: true, // true for 465, false for other ports
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
